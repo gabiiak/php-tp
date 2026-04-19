a529dfa1be6ee8cde8c9cd5f9769de15930d1bf9
gabii.ak <elpollokawaiicomecoco@gmail.com>
2026-04-19 18:41:44 -0300

    aña

---
+++ simulacro_parcial/array.php
@@ -0,0 +1,47 @@
+<?php
+
+use Soap\Url;
+
+    $array = array(
+        1 => array(
+            "codigo" => "1234",
+            "imagen" => "../img/reloj.jpg",
+            "descripcion" => "Reloj Led Pulsera Deportivo Ajustable",
+            "stockActual" => "30",
+            "stockMinimo" => "20" ,
+            "precio" => "2446.5 "
+        ),
+        2 => array(
+            "codigo" => "2345",
+            "imagen" => "../img/auriculares1.jpg",
+            "descripcion" => "Auriculares in-ear Inalámbricos negros",
+            "stockActual" => "40",
+            "stockMinimo" => "20" ,
+            "precio" => "20996.5"
+        ),
+        3 => array(
+            "codigo" => "3456",
+            "imagen" => "../img/auriculares2.jpg",
+            "descripcion" => "Auriculares Inalámbricos blanco",
+            "stockActual" => "20",
+            "stockMinimo" => "25" ,
+            "precio" => "16796.5"
+        ),
+        4 => array(
+            "codigo" => "4567",
+            "imagen" => "../img/soporte.jpg",
+            "descripcion" => "Soporte Extensible Apoya Celular Para Notebook",
+            "stockActual" => "80",
+            "stockMinimo" => "40" ,
+            "precio" => "4196.5"
+        ),
+        5 => array(
+            "codigo" => "5678",
+            "imagen" => "../img/funda.jpg",
+            "descripcion" => "Funda Deportiva Para Iphone Brazalete",
+            "stockActual" => "100",
+            "stockMinimo" => "50" ,
+            "precio" => "16796.5"
+        )
+    );
+?>
